b71257abcdbcf2b3553edff58a3e7bd1bc317bb3
DazzlingFame <narsel1995@mail.ru>
2020-06-09 15:44:16 +0300

    added more images + hover styles

--- src/screens/resume/components/constants.ts
+++ src/screens/resume/components/constants.ts
@@ -1,6 +1,15 @@
 import logo from '../../../assets/images/doge.jpg'
+import smilingDoge from '../../../assets/images/dogeSmiling.jpg'
+import dogePillow from '../../../assets/images/dogePillow.jpg'
 
-export const TABLIO_TEXT = 'Tablio content text content text content text content text content text content text content text content text\n content text content text content text\n content textcontent textcontent text content text content text  content text content text content text\n content text content text content text\n content textcontent textcontent text content text conten  content text content text content text\n content text content text content text\n content textcontent textcontent text content text conten';
+export const TABLIO_TEXT = 'Tablio content text content text content text content text content text content text content text content text\n\n\n content text content text content text\n content textcontent textcontent text content text content text  content text content text content text\n content text content text content text\n content textcontent textcontent text content text conten  content text content text content text\n content text content text content text\n content textcontent textcontent text content text conten';
+
 export const DOGE_IMG = {
     source: logo, desc: 'doge'
+};
+export const DOGE_SMILING_IMG = {
+    source: smilingDoge, desc: 'smilingDoge'
+};
+export const DOGE_PILLOW_IMG = {
+    source: dogePillow, desc: 'dogePillow'
 };
--- src/screens/resume/components/tablioContent.tsx
+++ src/screens/resume/components/tablioContent.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import logo from '../../../assets/images/doge.jpg';
+import dogePillow from '../../../assets/images/dogePillow.jpg';
 import './tablioContentStyles.css'
-import {DOGE_IMG, TABLIO_TEXT} from "./constants";
+import {DOGE_IMG, DOGE_PILLOW_IMG, DOGE_SMILING_IMG, TABLIO_TEXT} from "./constants";
 
 const photosArray: Array<{source: string, desc: string}> = [
-    DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG, DOGE_IMG
+    DOGE_IMG, DOGE_SMILING_IMG, DOGE_PILLOW_IMG, DOGE_SMILING_IMG, DOGE_SMILING_IMG, DOGE_PILLOW_IMG, DOGE_IMG, DOGE_IMG, DOGE_SMILING_IMG, DOGE_SMILING_IMG, DOGE_PILLOW_IMG, DOGE_IMG
 ];
 
 export const TablioContent: React.FC = () => {
     const photos = photosArray.map(photo => (
-        <img src={photo.source} alt={photo.desc} style={{width: '30%', marginRight: 20, marginTop: 20}}/>
+        <img src={photo.source} alt={photo.desc} style={{width: '30%', marginRight: 20, marginTop: 20, objectFit: 'contain'}}/>
     ));
 
     return (
         <div className={'tablio_content_container'}>
             <div className={'tablio_content_header_container'}>
-                <img src={logo} alt={'doge'} className={'tablio_content_logo'}/>
+                <img src={dogePillow} alt={'doge'} className={'tablio_content_logo'}/>
                 <p className={'small_text'}>
                     {TABLIO_TEXT}
                 </p>
